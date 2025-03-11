955be48af4fc7d44ebfa99b27476e2917951f91f
4chyuth-j <achyuthj2002@gmail.com>
2025-03-11 17:31:20 +0530

    admin dashboard setup

--- controller/admin/adminController.js
+++ controller/admin/adminController.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
 
-
+// loading login page of admin
 const loadLogin = async (req,res)=>{
     if(req.session.admin){
         return res.redirect("/admin/dashboard");
@@ -12,9 +12,57 @@ const loadLogin = async (req,res)=>{
 }
 
 
+// login post 
+const login = async (req,res)=>{
+    try {
+        const {email,password} = req.body;
+        const admin = await User.findOne({email,isAdmin:true});
+        console.log(email);
+        if(!admin){
+            return res.render("admin-login",{message:"Admin don't exist!"});
+        }
+        if(admin){
+            const passwordMatch = await bcrypt.compare(password,admin.password);
+            console.log("passwordMatch:",passwordMatch);
+            if(!passwordMatch){
+                console.log("password don't match");
+                return res.render("admin-login.ejs",{message:"Incorrect password. Please try again."});
+                
+            }
+            
+            if(passwordMatch){
+                
+                console.log("password matched successfully.");
+                req.session.admin= true;
+                return res.redirect("/admin/")
+            } else{
+                return res.redirect("/login");
+            }
+        }
+
+    } catch (error) {
+         console.log("login error:",error);
+         return res.redirect("/pageError");
+    }
+}
+
+
+//load Dashboard
+const loadDashboard = async (req,res)=>{
+    try {
+        if(req.session.admin){
+            res.render("dashboard");
+        }
+    } catch (error) {
+        console.log("Error in loading the dashboard");
+        res.redirect("/pageError");
+    }
+}
 
 
 module.exports = {
     loadLogin,
+    login,
+    loadDashboard,
 
 }
